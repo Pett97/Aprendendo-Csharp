32aa5868dd9f21d4e19a8fa102de3d247d60b956
Pett97 <henriquepeterson138@gmail.com>
2025-06-20 21:04:22 -0300

    Aprendendo C#

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+Random random = new Random();
+
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration <= 10 && daysUntilExpiration >= 6)
+{
+   Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (daysUntilExpiration <= 5 && daysUntilExpiration != 1)
+{
+   Console.Write($"Your subscription expires in {daysUntilExpiration}.");
+   Console.WriteLine("Renew now and save 10%!");
+}
+
+if (daysUntilExpiration == 1)
+{
+   Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
+}
+
+if (daysUntilExpiration >= 10)
+{
+   Console.WriteLine("Your subscription has expired.");
+}
+
